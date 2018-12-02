6e03bf6c569af6b9ce30029b84dec72f237580f6
pansyjia <siyujia@umich.edu>
2018-12-01 19:09:38 -0500

    conflits removed

--- src/pages/chart/chart.ts
+++ src/pages/chart/chart.ts
@@ -36,26 +36,7 @@ export class ChartPage {
  public chartLoadingEl: any;
 
 
-<<<<<<< HEAD
-=======
-
-  constructor(public navCtrl: NavController,
-              public navParams: NavParams,
-              private entryService: EntryDataServiceProvider) {
-    this.entryService.getObservable().subscribe(
-      (update) => {
-          this.entries = this.entryService.getEntries();
-          console.log(this.entries);
-        console.log('this.entryService.getObservable().subscribe ');
-      },
-      (err) => {
-        console.log('this.entryService.getObservable().subscribe :', err);
-      });
-    this.entries = this.entryService.getEntries();
-
->>>>>>> 0c599a7ceacddb310839e8bb8f3196d11c7e572d
-
-  constructor(public navCtrl: NavController,
+constructor(public navCtrl: NavController,
               public navParams: NavParams,
               private entryService: EntryDataServiceProvider) {
 
@@ -88,14 +69,9 @@ export class ChartPage {
       let k : any;
       for(k in this.entries)
       {
-<<<<<<< HEAD
          var entry = this.entries[k];
          let thisMood = this.entryService.getMood(entry.mood);
          console.log("retrieved mood:", thisMood);
-=======
-         var entry = k;
-         let thisMood = this.entryService.getMood(entry.mood)
->>>>>>> 0c599a7ceacddb310839e8bb8f3196d11c7e572d
          this.chartLabels.push(entry.location);
          this.chartValues.push(thisMood.score);
          this.chartColours.push(thisMood.color);
